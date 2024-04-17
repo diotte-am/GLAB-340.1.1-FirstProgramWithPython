2cc596a71fee483c47424d45635c28c6216b3641
Amare Diotte <83765247+diotte-am@users.noreply.github.com>
2024-04-17 16:23:01 -0400

    340.1.1 -First program with python

---
+++ diotte_am_hello.py
@@ -0,0 +1,22 @@
+# Amare Diotte
+# 2024-RTT-107
+# GLAB-340.1.1
+
+# constant
+AMARE_AGE = 37
+
+# function containing hello world message
+def greeting():
+    # inputs saved to variables
+    user_name = input("What's your name? ")
+    user_age = int(input("How old are you? "))
+    # conditionals
+    if user_age == AMARE_AGE:
+        print("Wow, %s, we're the same age!", user_name)
+    elif user_age > AMARE_AGE:
+        print("Hi, " + user_name + ", you are " + str(user_age - AMARE_AGE) + " years older than me")
+    else:
+        print("Hi, " + user_name + ", you are " + str(AMARE_AGE - user_age) + " years younger than me")
+
+
+greeting()
